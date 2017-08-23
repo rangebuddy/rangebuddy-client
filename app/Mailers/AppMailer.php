c4284a320acbb70eb1a9439343ac9c218841232e
msjmeyer <mmeyer@tradeninja.net>
2017-08-23 16:42:53 -0400

    Update AppMailer.php

--- app/Mailers/AppMailer.php
+++ app/Mailers/AppMailer.php
@@ -20,7 +20,7 @@ class AppMailer
     public function sendEmailConfirmationTo(User $user)
     {
         Mail::send('emails.confirm', ['user' => $user], function ($message) use ($user) {
-            $message->from(env('MAIL_NOREPLY', 'noreply@example.com'), env('APP_NAME', 'Laravel'))
+            $message->from(env('MAIL_NOREPLY', 'accounts@rangebuddy.ca'), env('APP_NAME', 'Laravel'))
             ->to($user->email, $user->name)
             ->subject('[' . env('APP_NAME') . '] ' . trans('messages.confirm_your_email'));
         });
