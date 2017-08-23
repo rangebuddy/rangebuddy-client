53ff64d411caffd9da323bb7667ca34c4a5658bc
msjmeyer <mmeyer@tradeninja.net>
2017-08-23 16:37:05 -0400

    Update invite default email

--- app/Http/Controllers/InviteController.php
+++ app/Http/Controllers/InviteController.php
@@ -111,7 +111,7 @@ class InviteController extends Controller
 
                 // - send invitation email
                 Mail::send('emails.invite', ['invite' => $invite, 'group' => $group, 'invitating_user' => $request->user()], function ($message) use ($email, $request, $group) {
-                    $message->from(env('MAIL_FROM', 'noreply@example.com'), env('APP_NAME', 'Laravel'))
+                    $message->from(env('MAIL_FROM', 'accounts@rangebuddy.ca'), env('APP_NAME', 'Laravel'))
                     ->to($email)
                     ->subject( '[' . env('APP_NAME') . '] ' . trans('messages.invitation_to_join') . ' "'   . $group->name . '"');
                 });
