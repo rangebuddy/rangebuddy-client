95220b6bd7d097326aac37772051c1c42e6ae001
msjmeyer <mmeyer@tradeninja.net>
2017-08-23 17:11:56 -0400

    Update InviteController.php

--- app/Http/Controllers/InviteController.php
+++ app/Http/Controllers/InviteController.php
@@ -111,7 +111,7 @@ class InviteController extends Controller
 
                 // - send invitation email
                 Mail::send('emails.invite', ['invite' => $invite, 'group' => $group, 'invitating_user' => $request->user()], function ($message) use ($email, $request, $group) {
-                    $message->from(env('MAIL_FROM', 'accounts@rangebuddy.ca'), env('APP_NAME', 'Rangebuddy'))
+                    $message->from(env('MAIL_FROM', 'accounts@rangebuddy.ca'), env('APP_NAME', 'Range Buddy'))
                     ->to($email)
                     ->subject( '[' . env('APP_NAME') . '] ' . trans('messages.invitation_to_join') . ' "'   . $group->name . '"');
                 });
